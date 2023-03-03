2a19e766821629a7e314a524a7ebd0fd90569845
Renato <deviconube@gmail.com>
2023-03-03 01:40:00 -0300

    code review - running prettier

--- force-app/main/default/lwc/fibonacci/fibonacci.js
+++ force-app/main/default/lwc/fibonacci/fibonacci.js
@@ -1,107 +1,76 @@
-import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-export default class Fibonacci extends LightningElement 
-{
-  visible = false;
-  num = 0;
-  resultadoFibo;
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+export default class Fibonacci extends LightningElement {
+    visible = false;
+    num = 0;
+    resultadoFibo;
 
-
-    pegarValor(event)
-    {
-      const nome = event.target.name;
-        if(nome === 'numero')
-        {
+    pegarValor(event) {
+        const nome = event.target.name;
+        if (nome === "numero") {
             this.num = event.target.value;
         }
     }
 
-
-    Fibo(n) 
-    {
-        if (n < 0) 
-        {
+    Fibo(n) {
+        if (n < 0) {
             return null;
-        } 
-        else if (n === 0)
-        {
+        } else if (n === 0) {
             return 0;
-        } 
-        else if (n === 1)
-        {
+        } else if (n === 1) {
             return 1;
-        } 
-        else
-        {
+        } else {
             return this.Fibo(n - 1) + this.Fibo(n - 2);
         }
     }
 
-    previous()
-    {
+    previous() {
         this.num--;
         this.resultadoFibo = this.Fibo(this.num);
     }
 
-    next() 
-    {
+    next() {
         this.num++;
         this.resultadoFibo = this.Fibo(this.num);
     }
 
-
-    handleClick()
-    {
-        
-        if (this.num.trim() < 0)
-        {
-        this.handleError();
-        }
-        else if (isNaN(this.num))
-        {
+    handleClick() {
+        if (this.num.trim() < 0) {
+            this.handleError();
+        } else if (isNaN(this.num)) {
             this.NaNError();
-        }
-        else if (!this.num.trim()) {
+        } else if (!this.num.trim()) {
             this.nullError();
-        }
-        else
-        {   
+        } else {
             this.visible = true;
-        this.resultadoFibo = this.Fibo(this.num.trim());
-        } 
+            this.resultadoFibo = this.Fibo(this.num.trim());
+        }
     }
 
-
-    handleError()
-    {
+    handleError() {
         const error = new ShowToastEvent({
-            title: 'Erro',
-            message: 'O número inserido não pode ser negativo.',
-            variant: 'error',
+            title: "Erro",
+            message: "O número inserido não pode ser negativo.",
+            variant: "error"
         });
         this.dispatchEvent(error);
-
-     }
-
-    NaNError()
-    {
-            const error = new ShowToastEvent({
-                title: 'Erro',
-                message: 'Por favor, insira somente números.',
-                variant: 'error',
-            });
-            this.dispatchEvent(error);
-        
     }
 
-    nullError()
-    {
-            const error = new ShowToastEvent({
-                title: 'Erro',
-                message: 'Por favor, insira um número.',
-                variant: 'error',
-            });
-            this.dispatchEvent(error);
-        
+    NaNError() {
+        const error = new ShowToastEvent({
+            title: "Erro",
+            message: "Por favor, insira somente números.",
+            variant: "error"
+        });
+        this.dispatchEvent(error);
+    }
+
+    nullError() {
+        const error = new ShowToastEvent({
+            title: "Erro",
+            message: "Por favor, insira um número.",
+            variant: "error"
+        });
+        this.dispatchEvent(error);
     }
 }
